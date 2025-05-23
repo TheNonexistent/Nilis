5275acac872819485659f73eebac3cf6d398890a
TheNonexistent <thenonexistent@protonmail.com>
2025-05-23 15:46:13 +0330

    feat(server): basic project scaffold, configuration and logging initialization
    created basic project structure, handled configuration loading using viper, logging using zerolog and provided a basic configuration example

---
+++ cmd/server/config.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/rs/zerolog/log"
+	"github.com/spf13/viper"
+)
+
+var defaults = map[string]any{
+	"server.listen_port":       6226,
+	"server.bind_address":      "0.0.0.0",
+	"server.database_location": "/opt/nilis/local.db",
+
+	"sharding.enabled":  false,
+	"sharding.shard_id": 0,
+	"sharding.replica":  false,
+	"sharding.shards":   []map[string]any{},
+
+	"logging.level": "info",
+	"logging.file":  "/var/log/nilis.log",
+}
+
+type Config struct {
+	Server struct {
+		ListenPort       int    `mapstructure:"listen_port"`
+		BindAddress      string `mapstructure:"bind_address"`
+		DatabaseLocation string `mapstructure:"database_location"`
+	} `mapstructure:"server"`
+
+	Sharding struct {
+		Enabled bool `mapstructure:"enabled"`
+		ShardID int  `mapstructure:"shard_id"`
+		Replica bool `mapstructure:"replica"`
+		Shards  []struct {
+			ID       int      `mapstructure:"id"`
+			Address  string   `mapstructure:"address"`
+			Replicas []string `mapstructure:"replicas"`
+		} `mapstructure:"shards"`
+	} `mapstructure:"sharding"`
+
+	Logging struct {
+		Level string `mapstructure:"level"`
+		File  string `mapstructure:"file"`
+	} `mapstructure:"logging"`
+}
+
+func loadConfig(config *Config) error {
+	v := viper.New()
+	v.SetConfigName("nilis")
+	v.SetConfigType("yaml")
+	v.AddConfigPath("/etc/nilis")
+	v.AddConfigPath(".")
+
+	for k, v := range defaults {
+		viper.SetDefault(k, v)
+	}
+
+	if err := v.ReadInConfig(); err != nil {
+		log.Warn().Err(err).Str("module", "configuration").Msg("could not read config file, using defaults...")
+		return nil
+	}
+
+	if err := v.Unmarshal(config); err != nil {
+		return fmt.Errorf("error unmarshaling config file: %w", err)
+	}
+
+	return nil
+}
---
+++ cmd/server/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+var config Config
+
+func main() {
+
+	if err := loadConfig(&config); err != nil {
+		log.Fatal().Err(err).Msg("error in initial configuration")
+	}
+
+	initLogger(config.Logging.Level)
+
+	fmt.Printf("%+v", config)
+}
+
+func initLogger(levelStr string) {
+	levelStr = strings.ToLower(levelStr)
+
+	level, err := zerolog.ParseLevel(levelStr)
+	if err != nil {
+		level = zerolog.InfoLevel
+		log.Warn().Str("module", "logging").Msgf("invalid log level '%s' specified, falling back to 'info'", levelStr)
+	}
+
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	zerolog.SetGlobalLevel(level)
+
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+
+}
