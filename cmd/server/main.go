1d65a334d022267bccc77d1d9c8e2380d7d88b71
TheNonexistent <thenonexistent@protonmail.com>
2025-05-23 19:17:31 +0330

    feat(server): grpc server bootstrap

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"os"
 	"strings"
 
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
+	"github.com/thenonexistent/nilis/pkg/store"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/reflection"
 )
 
 var config Config
@@ -14,16 +19,70 @@ var config Config
 func main() {
 
 	if err := loadConfig(&config); err != nil {
-		log.Fatal().Err(err).Msg("error in initial configuration")
+		log.Fatal().Str("module", "main").Err(err).Msg("error in initial configuration")
 	}
 
 	if err := validateConfig(&config); err != nil {
-		log.Fatal().Err(err).Msg("invalid configuration")
+		log.Fatal().Str("module", "main").Err(err).Msg("invalid configuration")
 	}
 
 	initLogger(config.Logging.Level)
 
-	fmt.Printf("%+v", config)
+	storeServer, cancelFunc, err := NewServer(&config)
+	if err != nil {
+		log.Fatal().Str("module", "main").Err(err).Msg("failed to create store server")
+	}
+	defer cancelFunc()
+
+	errChan := make(chan error, 2)
+	sigChan := make(chan os.Signal, 1)
+
+	go func() {
+		if err := startGRPCServer(storeServer); err != nil {
+			errChan <- fmt.Errorf("gRPC server failed: %w", err)
+		}
+	}()
+
+	select {
+	case sig := <-sigChan:
+		log.Info().Str("signal", sig.String()).Msg("received interrupt, exiting...")
+		os.Exit(1)
+
+	case err := <-errChan:
+		log.Fatal().Str("module", "main").Err(err).Msg("store service failed")
+	}
+}
+
+func startGRPCServer(storeServer *Server) error {
+	ListenAddr := fmt.Sprintf("%s:%d", config.Server.BindAddress, config.Server.ListenPort)
+	lis, err := net.Listen("tcp", ListenAddr)
+	if err != nil {
+		return fmt.Errorf("failed to listen on %s: %w", ListenAddr, err)
+	}
+
+	srvOpts := []grpc.ServerOption{}
+
+	srvOpts = append(srvOpts, grpc.UnaryInterceptor(UnaryCancelInterceptor))
+
+	if strings.ToLower(config.Logging.Level) == "debug" {
+		srvOpts = append(srvOpts, grpc.UnaryInterceptor(UnaryLoggingInterceptor))
+	}
+
+	if config.Server.UseTLS {
+		creds, err := credentials.NewServerTLSFromFile(config.Server.TLSCert, config.Server.TLSKey)
+		if err != nil {
+			return fmt.Errorf("failed to load certificates: %w", err)
+		}
+
+		srvOpts = append(srvOpts, grpc.Creds(creds))
+	}
+
+	s := grpc.NewServer(srvOpts...)
+	reflection.Register(s)
+	store.RegisterStoreServer(s, storeServer)
+
+	log.Info().Str("module", "grpc").Str("listen_address", ListenAddr).Msg("gRPC server started")
+	return s.Serve(lis)
 }
 
 func initLogger(levelStr string) {
--- go.mod
+++ go.mod
@@ -3,23 +3,30 @@ module github.com/thenonexistent/nilis
 go 1.24.3
 
 require (
+	github.com/rs/zerolog v1.34.0
+	github.com/spf13/viper v1.20.1
+	go.etcd.io/bbolt v1.4.0
+	google.golang.org/grpc v1.67.3
+	google.golang.org/protobuf v1.36.1
+)
+
+require (
 	github.com/fsnotify/fsnotify v1.8.0 // indirect
 	github.com/go-viper/mapstructure/v2 v2.2.1 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.3 // indirect
-	github.com/rs/zerolog v1.34.0 // indirect
 	github.com/sagikazarmark/locafero v0.7.0 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
 	github.com/spf13/afero v1.12.0 // indirect
 	github.com/spf13/cast v1.7.1 // indirect
 	github.com/spf13/pflag v1.0.6 // indirect
-	github.com/spf13/viper v1.20.1 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
-	go.etcd.io/bbolt v1.4.0 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.9.0 // indirect
+	golang.org/x/net v0.33.0 // indirect
 	golang.org/x/sys v0.33.0 // indirect
 	golang.org/x/text v0.21.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20241223144023-3abc09e42ca8 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
