f54423a4d89e17e1d53f241ba6dc5b8c09b935db
TheNonexistent <thenonexistent@protonmail.com>
2025-05-23 17:25:28 +0330

    feat(database): initial database struct and methods, a wrapper for boltdb

---
+++ internal/db/db.go
@@ -0,0 +1,54 @@
+package db
+
+import bolt "go.etcd.io/bbolt"
+
+const defaultBucketName = "nilis"
+
+type Database struct {
+	database *bolt.DB
+}
+
+func NewDatabase(path string) (*Database, error) {
+	database, err := bolt.Open(path, 0600, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Database{
+		database: database,
+	}, nil
+}
+
+func (db *Database) SetKey(key string, value []byte) error {
+	return db.database.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(defaultBucketName))
+		return b.Put([]byte(key), value)
+	})
+}
+
+func (db *Database) GetKey(key string) ([]byte, error) {
+	var value []byte
+
+	err := db.database.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(defaultBucketName))
+		value = b.Get([]byte(key))
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return value, nil
+}
+
+func (db *Database) DeleteKey(key string) error {
+	return db.database.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(defaultBucketName))
+		return b.Delete([]byte(key))
+	})
+}
+
+func (db *Database) Close() error {
+	return db.database.Close()
+}
